2c12a1b069ec7f9966259c8d34054317060ea69f
Vinicius Serra <vinicius.serra14@gmail.com>
2023-10-30 21:06:20 -0300

    iniciando

---
+++ src/main/java/br/com/ATS/Controller/AuthController.java
@@ -0,0 +1,6 @@
+package br.com.ATS.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
---
+++ src/test/java/br/com/ATS/AtsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ATS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
